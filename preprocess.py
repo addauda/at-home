2113d31a618070f8ca927a4667202de64702d9e7
Ahmed Dauda <ahmed.dauda@uoit.net>
2020-01-31 15:08:58 -0500

    improved comments

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,7 @@ import re
 PREFIX = { 'K': 10**3, 'M': 10**6 }
 OUTPUT_FILE_NAME = "./data/preprocessed_challenge_data.csv"
 
-## maps a humanized price e.g 1.25M to a float e.g 1,250,000.0 for search/filtering purposess
+## Maps a humanized price e.g 1.25M to a float e.g 1,250,000.0 for search/filtering purposess
 def humanized_to_float (humanized_price):
 	price_components = re.findall(r'[\d\.\d]+|[KM]', humanized_price)
 	return float(price_components[0]) * PREFIX[price_components[1]]
@@ -14,7 +14,7 @@ def humanized_to_float (humanized_price):
 zillow_df = pd.read_csv("./raw/challenge_data.csv", parse_dates=True)
 zillow_df.set_index('zillow_id', inplace=True)
 
-## Change humanized price to float
+## Map humanized price to float
 zillow_df['price'] = zillow_df['price'].map(humanized_to_float)
 
 ## Write out new csv
